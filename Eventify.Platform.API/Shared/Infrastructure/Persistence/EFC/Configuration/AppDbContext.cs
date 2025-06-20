f4817c0a4af6b57f532fcb84e6e1a49f79cc29a0
Dacc03 <u202120569@upc.edu.pe>
2025-06-19 20:14:45 -0500

    feat(profiles): add profiles context configuration to AppDbContext.

--- Eventify.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ Eventify.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Eventify.Platform.API.Profiles.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Eventify.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,9 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        //Apply configuration for the Profiles bounded context
+        builder.ApplyProfilesConfiguration();
         
         // Use snake case naming convention for the database
         builder.UseSnakeCaseNamingConvention();
