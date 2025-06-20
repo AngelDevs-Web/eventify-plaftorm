3ba8412009acf284715566d146f9f5c85a0da3e5
Dacc03 <u202120569@upc.edu.pe>
2025-06-19 20:08:45 -0500

    feat(profiles): add profile resource.

---
+++ Eventify.Platform.API/Profiles/Interfaces/REST/Resources/ProfileResource.cs
@@ -0,0 +1,7 @@
+﻿namespace Eventify.Platform.API.Profiles.Interfaces.REST.Resources;
+
+public record ProfileResource(
+    int Id,
+    string FullName,
+    string Email,
+    string StreetAddress);
