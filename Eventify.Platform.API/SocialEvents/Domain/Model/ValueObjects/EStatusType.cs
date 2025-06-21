172047786bd168b14550cc335502ebd8bbd2c75e
JulyP <u20211d760@upc.edu.pe>
2025-06-21 13:29:06 -0500

    feat(social-event): add enum EStatusType.

---
+++ Eventify.Platform.API/SocialEvents/Domain/Model/ValueObjects/EStatusType.cs
@@ -0,0 +1,11 @@
+namespace Eventify.Platform.API.SocialEvents.Domain.Model.ValueObjects;
+/// <summary>
+/// Represents the status of a social event.
+/// </summary>
+public enum EStatusType
+{
+    Active,
+    ToConfirm,
+    Cancelled,
+    Completed
+}
