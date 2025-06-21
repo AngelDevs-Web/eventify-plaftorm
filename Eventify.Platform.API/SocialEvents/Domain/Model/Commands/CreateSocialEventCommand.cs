04ea23e2a775679fedf644fd8f1bbc9eb4de0ef0
JulyP <u20211d760@upc.edu.pe>
2025-06-21 13:34:16 -0500

    feat(social-event): add CreateSocialEventCommand.

---
+++ Eventify.Platform.API/SocialEvents/Domain/Model/Commands/CreateSocialEventCommand.cs
@@ -0,0 +1,11 @@
+using Eventify.Platform.API.SocialEvents.Domain.Model.ValueObjects;
+
+namespace Eventify.Platform.API.SocialEvents.Domain.Model.Commands;
+
+public record CreateSocialEventCommand(
+    string EventTitle,
+    DateTime EventDate,
+    string CustomerName,
+    string Location,
+    EStatusType Status
+    );
