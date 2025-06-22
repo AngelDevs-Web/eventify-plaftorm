53843aa5283ea3fd0b679018fca68fda5d3ee09e
OmBRz <omarbrrcl@gmail.com>
2025-06-22 02:42:43 -0500

    feat(review): add domain layer.

---
+++ Eventify.Platform.API/Operation/Domain/Model/Aggregates/Review.cs
@@ -0,0 +1,30 @@
+﻿using Eventify.Platform.API.Operation.Domain.Model.Commands;
+using Eventify.Platform.API.Operation.Domain.Model.ValueObjects;
+
+namespace Eventify.Platform.API.Operation.Domain.Model.Aggregates;
+
+public partial class Review
+{
+    public int Id { get; }
+    public string Content { get; private set; }
+    public int Rating { get; private set; }
+    public ProfileId ProfileId { get; private set; }
+    public SocialEventId SocialEventId { get; private set; }
+
+    public Review(string content, int rating, ProfileId profileId, SocialEventId socialEventId)
+    {
+        Content = content;
+        Rating = rating;
+        ProfileId = profileId;
+        SocialEventId = socialEventId;
+    }
+
+    public Review(CreateReviewCommand command) : this( 
+        command.Content, 
+        command.Rating, 
+        command.ProfileId, 
+        command.SocialEventId)
+    { }
+    
+    
+}
---
+++ Eventify.Platform.API/Operation/Domain/Model/Commands/CreateReviewCommand.cs
@@ -0,0 +1,10 @@
+﻿using Eventify.Platform.API.Operation.Domain.Model.ValueObjects;
+
+namespace Eventify.Platform.API.Operation.Domain.Model.Commands;
+
+public record CreateReviewCommand(
+    String Content,
+    int Rating,
+    ProfileId ProfileId,
+    SocialEventId SocialEventId
+    );
