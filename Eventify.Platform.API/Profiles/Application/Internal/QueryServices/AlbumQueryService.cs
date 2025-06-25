c1285750867cd3c8a125c2f918eccb484e1651eb
Deybbi Anderson Crisanto Calle <u202120569@upc.edu.pe>
2025-06-24 19:28:43 -0500

    feat(profiles-albums): add AlbumQueryService for album data retrieval.

---
+++ Eventify.Platform.API/Profiles/Application/Internal/QueryServices/AlbumQueryService.cs
@@ -0,0 +1,21 @@
+using Eventify.Platform.API.Profiles.Albums.Domain.Model.Aggregates;
+using Eventify.Platform.API.Profiles.Domain.Model.Queries;
+using Eventify.Platform.API.Profiles.Domain.Repositories;
+using Eventify.Platform.API.Profiles.Domain.Services;
+
+namespace Eventify.Platform.API.Profiles.Application.Internal.QueryServices;
+
+public class AlbumQueryService(
+    IAlbumRepository albumRepository
+) : IAlbumQueryService
+{
+    public async Task<IEnumerable<Album>> Handle(GetAllAlbumsQuery query)
+    {
+        return await albumRepository.ListAsync();
+    }
+
+    public async Task<Album?> Handle(GetAlbumByIdQuery query)
+    {
+        return await albumRepository.FindByIdAsync(query.AlbumId);
+    }
+}
