ba2f8138b2b8d787fe05b12baccb5e12ce0f3dc2
Dacc03 <u202120569@upc.edu.pe>
2025-06-27 16:49:43 -0500

    feat(profiles-management): add methods for retrieving albums by profile id in IAlbumRepository

--- Eventify.Platform.API/Profiles/Domain/Repositories/IAlbumRepository.cs
+++ Eventify.Platform.API/Profiles/Domain/Repositories/IAlbumRepository.cs
@@ -5,4 +5,6 @@ namespace Eventify.Platform.API.Profiles.Domain.Repositories;
 
 public interface IAlbumRepository : IBaseRepository<Album>
 {
+    Task<IEnumerable<Album>> ListByProfileIdAsync(int profileId);
+    Task<Album?> FindByIdAndProfileIdAsync(int albumId, int profileId);
 }
