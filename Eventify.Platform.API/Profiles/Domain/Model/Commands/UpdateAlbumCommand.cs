8865cc8a431f15825658b012d43803461417688f
Dacc03 <u202120569@upc.edu.pe>
2025-06-27 16:49:36 -0500

    feat(profiles-management): add ProfileId to UpdateAlbumCommand for profile-specific updates

--- Eventify.Platform.API/Profiles/Domain/Model/Commands/UpdateAlbumCommand.cs
+++ Eventify.Platform.API/Profiles/Domain/Model/Commands/UpdateAlbumCommand.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
  * Represents a command to update an album.
  */
 public record UpdateAlbumCommand(
+    int ProfileId,
     int AlbumId,
     string Name,
     IEnumerable<string> Photos);
