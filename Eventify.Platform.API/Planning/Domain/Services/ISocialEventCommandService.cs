2b66b593b2512a939b9ba697f1efb0e646cb2420
JulyP <u20211d760@upc.edu.pe>
2025-06-28 04:31:03 -0500

    refactor(events-management): move ISocialEventCommandService to Planning.

--- Eventify.Platform.API/Planning/Domain/Services/ISocialEventCommandService.cs
+++ Eventify.Platform.API/Planning/Domain/Services/ISocialEventCommandService.cs
@@ -1,7 +1,7 @@
-using Eventify.Platform.API.SocialEvents.Domain.Model.Aggregates;
-using Eventify.Platform.API.SocialEvents.Domain.Model.Commands;
+using Eventify.Platform.API.Planning.Domain.Model.Aggregates;
+using Eventify.Platform.API.Planning.Domain.Model.Commands;
 
-namespace Eventify.Platform.API.SocialEvents.Domain.Services;
+namespace Eventify.Platform.API.Planning.Domain.Services;
 
 public interface ISocialEventCommandService
 {
