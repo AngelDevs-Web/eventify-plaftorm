5692f995cc47ff543d9d56240082e0ed88c1e7e8
JulyP <u20211d760@upc.edu.pe>
2025-06-28 04:14:36 -0500

    refactor(social-event): move EStatusType value object to Planning namespace.

--- Eventify.Platform.API/Planning/Domain/Model/ValueObjects/EStatusType.cs
+++ Eventify.Platform.API/Planning/Domain/Model/ValueObjects/EStatusType.cs
@@ -1,4 +1,4 @@
-namespace Eventify.Platform.API.SocialEvents.Domain.Model.ValueObjects;
+namespace Eventify.Platform.API.Planning.Domain.Model.ValueObjects;
 /// <summary>
 /// Represents the status of a social event.
 /// </summary>
