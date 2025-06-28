2d70aac6d920dd2f34f197083eb3a4f710122029
JulyP <u20211d760@upc.edu.pe>
2025-06-28 04:30:07 -0500

    refactor(events-management): rename ISocialEventContextFacade and move to Planning.

--- Eventify.Platform.API/Planning/Interfaces/ACL/ISocialEventContextFacade.cs
+++ Eventify.Platform.API/Planning/Interfaces/ACL/ISocialEventContextFacade.cs
@@ -1,11 +1,12 @@
-using Eventify.Platform.API.SocialEvents.Domain.Model.Aggregates;
-using Eventify.Platform.API.SocialEvents.Domain.Model.ValueObjects;
+using Eventify.Platform.API.Planning.Domain.Model.Aggregates;
+using Eventify.Platform.API.Planning.Domain.Model.ValueObjects;
 
-namespace Eventify.Platform.API.SocialEvents.Interfaces.ACL;
+namespace Eventify.Platform.API.Planning.Interfaces.ACL;
 
 public interface ISocialEventContextFacade
 {
-    Task<int> CreateSocialEventAsync(string eventTitle, DateTime eventDate, string customerName, string location, EStatusType status);
+    Task<string> CreateSocialEventAsync(string eventTitle, DateTime eventDate, string customerName, string location,
+        EStatusType status);
     Task<SocialEvent?> GetSocialEventByIdAsync(int socialEventId);
     Task<IEnumerable<SocialEvent>> GetAllSocialEventsAsync();
     Task<bool> UpdateSocialEventAsync(int socialEventId, string eventTitle, DateTime eventDate, string customerName, string location, EStatusType status);
