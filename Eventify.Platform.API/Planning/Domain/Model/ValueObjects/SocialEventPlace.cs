1c31075d16367e55493f9fccfc8bfbe2ecd1a943
JulyP <u20211d760@upc.edu.pe>
2025-06-28 04:11:58 -0500

    refactor(social-event): refactor directory for SocialEventPlace value object.

--- Eventify.Platform.API/Planning/Domain/Model/ValueObjects/SocialEventPlace.cs
+++ Eventify.Platform.API/Planning/Domain/Model/ValueObjects/SocialEventPlace.cs
@@ -1,4 +1,4 @@
-namespace Eventify.Platform.API.SocialEvents.Domain.Model.ValueObjects;
+namespace Eventify.Platform.API.Planning.Domain.Model.ValueObjects;
 
 /** * Represents the place where a social event is held.
  * This value object encapsulates the place string to ensure that each social event has a specific place.
